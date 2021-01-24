b4852c7618c600da3e4d0eb344ad6ef5dc7af45a
palo1992 <65390185+palo1992@users.noreply.github.com>
2021-01-24 15:34:46 +0100

    ready to deploy

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 //  Requiring packages
-const dotenv = require('dotenv').config(),
-    express = require("express"),
-    flash = require("connect-flash"),
-    bodyParser = require("body-parser"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    app = express();
+const express = require("express"),
+  flash = require("connect-flash"),
+  bodyParser = require("body-parser"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv = require("dotenv").config();
+}
 
 //  Requiring models
 var Campground = require("./models/campground"),
-    Comment = require("./models/comment"),
-    User = require("./models/user");
+  Comment = require("./models/comment"),
+  User = require("./models/user");
 //  Seeding Database Requiring
 const seedDB = require("./seeds");
 //  Requiring routes
 var commentRoutes = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes = require("./routes/index");
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 //  SETTINGS
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,40 +33,45 @@ app.use(flash());
 //seedDB();
 
 //  PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "TridsatOsem sposobov ako si pripravit skvelu kavu",
+app.use(
+  require("express-session")({
+    secret: process.env.SEC_KEY,
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 //  USER ARGUMENT PASS OVER..
-app.use(async function(req, res, next) {
-    res.locals.currentUser = req.user;
-    if (req.user) {
-        try {
-            let user = await User.findById(req.user._id).populate('notifications', null, { isRead: false }).exec();
-            res.locals.notifications = user.notifications.reverse();
-        } catch (err) {
-            console.log(err.message);
-        }
+app.use(async function (req, res, next) {
+  res.locals.currentUser = req.user;
+  if (req.user) {
+    try {
+      let user = await User.findById(req.user._id)
+        .populate("notifications", null, { isRead: false })
+        .exec();
+      res.locals.notifications = user.notifications.reverse();
+    } catch (err) {
+      console.log(err.message);
     }
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+  }
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 //  Mongoose Connection
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
+const dbUrl = process.env.DB_URL || "mongodb://localhost/yelp_camp";
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 //  LOADING PAGES
 app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
-//  SET UP PORT 
+//  SET UP PORT
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
-    console.log("YelpCamp app has started");
+  console.log("YelpCamp app has started");
 });
